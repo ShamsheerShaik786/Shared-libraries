47a0e4e75a775e882426b2bd4eb135004181e480
ShamsheerShaik786 <shamsheersk07@gmail.com>
2024-11-14 20:10:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,16 @@
-
+def Download(repo)
+{
+  git "https://github.com/ShamsheerShaik786/${repo}.git"
+}
+def Build()
+{
+  sh 'mvn package'
+}
+def Deploy(jobname,IP,Contextpath)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat10/webapps/${Contextpath}.war'
+}
+def RunTestcases(repo)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
